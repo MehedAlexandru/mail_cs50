1979c3404a93a5eeb716bef17be6bdd113b249aa
Dio <alex.mehedinteanu.95@gmail.com>
2023-11-27 20:08:58 +0200

    New features: 	- added reply functionality 	- fixed bug where quotes inside the body of the email caused an 	  error

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+
   // By default, load the inbox
   load_mailbox('inbox');
 
@@ -18,6 +19,7 @@ function compose_email() {
   document.querySelector('#compose-view').style.display = 'block';
 
   send_email();
+  // compose_replay();
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -47,6 +49,59 @@ function load_mailbox(mailbox) {
 }
 
 
+// Custom function to unescape single quotes
+function unescapeSingleQuotes(str) {
+  return str.replace(/%27/g, "'");
+}
+
+
+function reply(encodedEmail) {
+  let email = JSON.parse(decodeURIComponent(unescapeSingleQuotes(encodedEmail)));
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#emails-view').style.display = 'none';
+
+  let recipients = email.sender;
+  let _subject = `Re: ${email.subject.replace(/['"]/g, '\\$&')} `;
+  let _body = `On ${email.timestamp} ${email.sender} wrote: ${email.body.replace(/['"]/g, '\\$&')} `;
+
+  console.log(recipients);
+  console.log(_subject);
+  console.log(_body);
+
+  let form = document.querySelector('#compose-form');
+  form.recipients = recipients;
+  form.subject = _subject;
+  form.body = _body;
+
+  form.querySelector('#compose-recipients').value = recipients;
+  form.querySelector('#compose-subject').value = _subject;
+  form.querySelector('#compose-body').value = _body;
+}
+
+document.querySelector('#compose-form').addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  let form = event.target;
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: form.recipients,
+      subject: form.subject,
+      body: form.body,
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(result => {
+      console.log(result);
+    });
+});
+
+
 function mailOpen(id) {
   console.log(id);
   fetch(`/emails/${id}`)
@@ -56,7 +111,7 @@ function mailOpen(id) {
       console.log(email);
       const mailView = document.querySelector('#emails-view')
       const composeView = document.querySelector('#compose-view')
-      composeView.innerHTML = '';
+      // composeView.innerHTML = '';
       mailView.innerHTML = '';
       // (`<div class="email card shadow p-3"><p>Sender: ${email.sender}</p></br><p>Subject: ${email.subject}</p></br><p>Body: ${email.body}</p></br></div>`);
       mailView.innerHTML =
@@ -66,7 +121,7 @@ function mailOpen(id) {
          <div class="">Body: ${email.body}</div></br>
          <div class="">Timestamp: ${email.timestamp}</div></br>
       </div>`;
-      if (email.read == false){
+      if (email.read == false) {
         fetch(`/emails/${id}`, {
           method: 'PUT',
           body: JSON.stringify({
@@ -75,12 +130,21 @@ function mailOpen(id) {
         });
       }
 
-      if (email.archived == false){
+      if (email.archived == false) {
         mailView.innerHTML += `<button class="btn btn-warning" onclick="archive(${email.id})">Archive</button>`;
-      }else{
+      } else {
         mailView.innerHTML += `<button class="btn btn-secondary" onclick="unarchive(${email.id})">Unarchive</button>`;
       }
-      
+
+      // mailView.innerHTML += `<button class="btn btn-primary mx-3" onclick='reply(${JSON.stringify(email)})'>Reply</button>`;
+      // Custom function to escape single quotes
+      function escapeSingleQuotes(str) {
+        return str.replace(/'/g, "%27");
+      }
+
+      // When creating the button, encode the email object and escape single quotes
+      mailView.innerHTML += `<button class="btn btn-primary mx-3" onclick='reply("${escapeSingleQuotes(encodeURIComponent(JSON.stringify(email)))}")'>Reply</button>`;
+
     });
 }
 
@@ -116,13 +180,13 @@ function load_inbox(mailView) {
       // Print emails
       console.log(emails);
       mailView.innerHTML = emails.map((email) => {
-      
-        return ( `<div onclick="mailOpen(${email.id})" class="row card" ${email.read ? 'style="background: grey"': ''}>
+
+        return (`<div onclick="mailOpen(${email.id})" class="row card" ${email.read ? 'style="background: grey"' : ''}>
          <div class="col-3">${email.sender}</div>
          <div class="col-6">${email.subject}</div>
          <div class="col-3">${email.timestamp}</div>
       </div>`);
-      
+
       });
 
     });
@@ -136,13 +200,13 @@ function load_archive(mailView) {
       // Print emails
       console.log(emails);
       mailView.innerHTML = emails.map((email) => {
-      
-        return ( `<div onclick="mailOpen(${email.id})" class="row card">
+
+        return (`<div onclick="mailOpen(${email.id})" class="row card">
          <div class="col-3">${email.sender}</div>
          <div class="col-6">${email.subject}</div>
          <div class="col-3">${email.timestamp}</div>
       </div>`);
-      
+
       });
 
     });
@@ -156,7 +220,6 @@ function load_sent(mailView) {
       // Print emails
       console.log(emails);
       mailView.innerHTML = emails.map((email) => {
-        // `<div class="email card shadow p-3"><p>Sender: ${email.sender}</p></br><p>Subject: ${email.subject}</p></br><p>Body: ${email.body}</p></br></div>`);
         return (
           `<div class="row">
          <div class="col-3">${email.sender}</div>
@@ -168,6 +231,8 @@ function load_sent(mailView) {
     });
 
 }
+
+
 
 
 function send_email() {
